67efb8acf4712705698f0fee1ae1b298e7ac8b59
peexthree <linedrop@yandex.ru>
2025-09-16 19:13:59 +0300

    ╨Ю╨▒╨╜╨╛╨▓╨╕╨╗ ╨║╨╛╨┤ ╨┐╤А╨╛╨╡╨║╤В╨░

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,8 +3,19 @@ import Link from 'next/link'
 
 export default function Logo() {
   return (
-    <Link href="/" aria-label="На главную">
-      <Image src="/logo.svg" alt="Лизинг" width={256} height={256} className="h-16 " />
+    <Link
+      href="/"
+      aria-label="На главную"
+      className="relative block h-16 w-16"
+    >
+      <Image
+        src="/logo.svg"
+        alt="Лизинг"
+        fill
+        sizes="4rem"
+        className="h-full w-full object-contain"
+        priority
+      />
     </Link>
   )
 }
