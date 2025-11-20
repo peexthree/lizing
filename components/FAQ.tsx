3f14c7cf2fb0eadb3cf857dfe80d4689ee718c8e
igorsychev1506 <igorsychev1506@gmail.com>
2025-11-20 11:17:31 +0000

    chore: trigger deployment

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState } from 'react' 
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
 import { motion, AnimatePresence } from 'framer-motion'
 
